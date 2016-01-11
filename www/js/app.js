992728a4f808726a82a80faa8d1f3e3aa5ec6509
niel <niel@geckotech.nl>
2016-01-11 12:27:26 +0200

    changed app name + fixed big overlapping header bar issue

--- www/js/app.js
+++ www/js/app.js
@@ -7,12 +7,12 @@ var popRateApp = angular.module('popRateApp', ['ionic','ionic.service.core', 'ng
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             cordova.plugins.Keyboard.disableScroll(true);
-
         }
         if (window.StatusBar) {
             // org.apache.cordova.statusbar required
             StatusBar.styleDefault();
         }
+        ionic.Platform.fullScreen();
     });
 })
 
